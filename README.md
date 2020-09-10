003e4ba0e1e0a235d84df8897a2ab221ecd629bf
Sitora Alexander <sitoraalexander@gmail.com>
2020-09-09 20:34:39 -0400

    Updated README, initiated and Pseudo coded index.html, script.js and style.css

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# wds
+# HW Assignment 5
+This is simple calendar application that allows you to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## Check out the URL of the deployed application here: 
+https://sitoraalexander.github.io/wds/
+
+## Here is the URL of the GitHub repository: 
+https://github.com/sitoraalexander/wds
+
---
+++ script.js
@@ -0,0 +1,16 @@
+//add current day and date with moments to #currentDay
+//function readFromLocalStorage ()
+//<!-- this is to look for anything saved previously in local storage -->
+   // use for loop
+       // - read value from time property
+       // - assign as text back in element (using its class (.description) and id (specific to each hour)
+       // - check current hour vs time slot (to determine color of text area: gray, red or white)
+           // - assign proper class (.past .present .future) based on comparison to current hour (,, === or =,=, ===)
+//function writeToLocalStorage ()
+//<!-- this is to save a value from the text area to local storage, using the hour as the key when save button is clicked -->
+    //add click event to save button class to run function
+        //- read text from text area (class = "description", should be parent of save button)
+        //- read schedule hour
+        //- trim text, trim schedule hour
+        //- send to local storage
+//call readFromLocalStorage(); //
